072a90218c8e578bb9533b35392fe3d049aa5948
shresta1801 <class.gattapugari@gmail.com>
2026-06-20 15:35:50 +0530

    Initial commit

---
+++ src/main/java/com/example/usersDemo/UserService.java
@@ -0,0 +1,31 @@
+package com.example.usersDemo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository us;
+
+
+    public UserService(UserRepository us) {
+        this.us = us;
+    }
+
+    public void save(User user) {
+        us.save(user);
+    }
+
+    public Optional<User> find(int id) {
+        return us.findById(id);
+    }
+
+    public List<User> findAll() {
+        return us.findAll();
+    }
+
+
+}
---
+++ src/test/java/com/example/usersDemo/UsersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
